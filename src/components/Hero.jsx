59b750d9b18703b0cc8850df7a7a253b9fdcfa95
kyle <kylepanuringan@gmail.com>
2026-06-16 00:32:51 +0800

    Add hero tailwind deisgn & add navbar tailwind responsive design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,33 @@
+import hero from "../assets/hero.png";
+
 function Hero() {
   return (
-    <section>
-        <h1>Welcome to the <br/>Public Domain<br/> Library</h1>
-        <p>A curated collection of timeless works from history's greatest minds. Read,explore and be inspired.</p>
-        <button>Browse Collection</button>
-    </section>
+<section 
+  style={{ 
+    backgroundImage: `url(${hero})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  }}
+  className="min-h-screen px-20 flex flex-col gap-2 justify-center"
+>
+  <div className="max-w-2xl">
+    <span className="text-white text-sm tracking-wider block">
+      WELCOME TO THE
+    </span>
+    <h1 className="text-white">
+      <div className="text-4xl md:text-5xl text-[#E5AB14]">Public Domain</div>
+      <div className="text-6xl md:text-7xl font-bold">Library</div>
+    </h1>
+    <p className="text-white text-base md:text-lg max-w-md mt-4">
+      A curated collection of timeless works from history's greatest minds.
+      <br />
+      Read, explore, and be inspired.
+    </p>
+    <a href="/browse" className="text-white text-base border border-white px-6 py-2 w-fit inline-block mt-6 hover:bg-white hover:text-slate-950 transition-colors">
+      Browse Collection
+    </a>
+  </div>
+</section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,106 @@
+
+import { useEffect, useRef, useState } from "react";
 import logo from "../assets/logo.svg";
+
 function Navbar() {
-  return (
-    <header className="flex items-center justify-around border-b border-[#E5AB14] p-4 bg-slate-950 text-amber-100">
-        <a href="/" className="flex item-center justify-center">
-            <img src={logo} alt="Public Domain Library Logo" className="w-30 h-auto"/>
-            <div className="flex item-center justify-center flex-col">
-                <div className="text-[#E5AB14] flex flex-col">
-                    <span className="text-xl uppercase tracking-wide -mb-2">Public Domain</span>
-                    <span className="font-bold text-4xl uppercase tracking-wider">Library</span>
+    const [searchClose, setSearchClose] = useState(true);
+    const [menuClose, setMenuClose] = useState(true);
+
+    const menuRef = useRef(null);
+
+    useEffect(()=>{
+        if (menuClose) return;
+
+        function handleClickOutside(event){
+            if(menuRef.current && !menuRef.current.contains(event.target)){
+                setMenuClose(true);
+            }
+        }
+
+        document.addEventListener("mousedown",handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown",handleClickOutside);
+        }
+    },[menuClose]);
+
+    return (
+        <header className="flex items-center border-b border-[#E5AB14] p-4 bg-slate-950 text-amber-100
+            justify-between lg:justify-evenly relative 
+        ">
+            {/* LEFT SIDE */}
+            <a href="/" className="flex items-center justify-center">
+                <img src={logo} alt="Public Domain Library Logo" 
+                    className="
+                        w-10 lg:w-30 
+                        h-10 lg:h-auto
+                    "
+                />
+                <div className="flex items-center justify-center flex-col">
+                    <div className="text-[#E5AB14] flex gap-1 uppercase
+                    flex-col
+                    ">
+                        <span className="-mb-2 text-xs lg:text-xl lg:tracking-wide">Public Domain</span>
+                        <span className="font-bold tracking-widest text-xl lg:text-4xl">Library</span>
+                    </div>
+                    <span className="hidden lg:block">Timeless works, for everyone.</span>
                 </div>
-                <span>Timeless works, for everyone.</span>
+            </a>
+
+            {/* RIGHT SIDE */}
+            <div className="flex gap-1 lg:hidden">
+                {searchClose 
+                ?
+                    <div>
+                        <button onClick={()=>{setSearchClose(false)}}>🔍</button>
+                        <button className="w-5" onClick={()=>{setMenuClose(!menuClose)}}>{menuClose ? "☰" : "✕"}</button>
+                    </div>
+                :
+                    <div className="flex">
+                        <form className="lg:block">
+                            <input 
+                                type="search" name="" id="" placeholder="Search..." 
+                                className="border border-black bg-white placeholder:italic placeholder-gray-500 px-2 text-black rounded"
+                            />
+                            <button>🔍</button>
+                        </form>  
+                        <button className="w-5" onClick={()=>{setSearchClose(true)}}>✕</button>
+                    </div>
+                }  
             </div>
-        </a>
 
-        <nav className="flex items-center gap-6"> 
-            <ul className="flex gap-6 items-center ">
-                <li><a href="">Home</a></li>
-                <li><a href="">Browse</a></li>
-                <li><a href="">Authors</a></li>
-                <li><a href="">About</a></li>
-            </ul>
-
-            <form className="border border-white">
-                <input type="search" name="" id="" className="border border-black"/>
-                <button>Search</button>
-            </form>
-        </nav>        
-    </header>
-  )
+            <div
+                ref={menuRef}
+                className={`
+                    ${menuClose ? 'hidden':'block'}
+                    absolute lg:static
+                    lg:flex items-center gap-6 
+                    bg-slate-950 top-full right-0 p-2 text-center
+                    border-l border-b border-r border-[#E5AB14] lg:border-none             
+            `}>
+                <nav> 
+                    <ul className="
+                        items-center gap-6 
+                        lg:flex 
+                        text-[#E5AB14]
+                        uppercase
+                    ">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Browse</a></li>
+                        <li><a href="">Authors</a></li>
+                        <li><a href="">About</a></li>
+                    </ul>
+                </nav>        
+                <form className="hidden lg:block">
+                    <input 
+                        type="search" name="" id="" placeholder="Search..." 
+                        className="border border-black bg-white placeholder:italic placeholder-gray-500 px-2 text-black rounded"
+                    />
+                    <button>🔍</button>
+                </form>
+            </div>
+        </header>
+    )
 }
 
 export default Navbar
