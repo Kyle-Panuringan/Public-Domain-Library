f04e90ce5b06e7aeb2049443afd25fec11de3a11
kyle <kylepanuringan@gmail.com>
2026-06-17 22:02:21 +0800

    Add css footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,34 @@
+import logo from "../assets/logo.svg";
+
 function Footer() {
   return (
-    <footer>
-      <nav>
-        <ul>
-          <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="">FAQ</a></li>
-        </ul>
-      </nav>
+    <footer className="bg-slate-950 text-white">
+      <div>
+        <p className="text-center p-2 border-b border-amber-400 italic">
+          <span>
+            "Books are legacies that a great genius leaves to mankind, 
+            which are delivered down from generation to generation, 
+            as presents to the posterity of those who are yet unborn."
+          </span><br />
+          <span>— Joseph Addison</span>
+        </p>
+      </div>
 
-      <p>{new Date().getFullYear()} Public Domain Library</p>
+      <div className="flex flex-col md:flex-row items-center justify-around items-center">
+        <nav>
+          <ul className="flex gap-3">
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">FAQ</a></li>
+          </ul>
+        </nav>
+
+        <img src={logo} alt="Public Domain Library Logo" 
+                            className="w-10 lg:w-20 h-10 lg:h-auto"
+        />
+
+        <p>{new Date().getFullYear()} Public Domain Library</p>
+      </div>
     </footer>
   )
 }
